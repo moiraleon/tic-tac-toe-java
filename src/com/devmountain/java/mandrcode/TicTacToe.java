1337ceef2b009cdb7c865d667ba707c4b95a7d0d
Moira Leon <moira.leon2@gmail.com>
2021-10-07 04:32:25 -0600

    cleaning code and creating new user and symbols

--- src/com/devmountain/java/mandrcode/TicTacToe.java
+++ src/com/devmountain/java/mandrcode/TicTacToe.java
@@ -1,15 +1,27 @@
 package com.devmountain.java.mandrcode;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
     public static void main(String[] args) {
-	char[][] gameBoard = {{' ', '|', ' ', '|',' '},
+	char[][] gameBoard = {{' ', '|', ' ', '|',' '},//2D array
             {'-', '+','-', '+','-'},
             {' ', '|', ' ', '|',' '},
             {'-', '+','-', '+','-'},
             {' ', '|', ' ', '|',' '}};
 //prints out game board
 		printGameBoard(gameBoard);
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your placement(1-9):");
+		int pos = scan.nextInt();
+		System.out.println(pos);
+
+		placePiece(gameBoard, pos, "player");
+
+		printGameBoard(gameBoard);
+
     }
 	    public static void printGameBoard(char[] [] gameBoard){
 		for (char[] row : gameBoard){
@@ -19,7 +31,49 @@ public class TicTacToe {
 			System.out.println();
 		}
 	}
+public static void placePiece(char[][] gameBoard, int pos, String user){
 
+    	char symbol = ' ';
+    	if (user.equals("player")){
+    		symbol = 'X';
+		}else if (user.equals("cpu")){
+    		symbol = 'O';
+		}
+
+
+	switch (pos){
+		case 1:
+			gameBoard[0][0] = symbol;
+			break;
+		case 2:
+			gameBoard[0][2] = symbol;
+			break;
+		case 3:
+			gameBoard[0][4] = symbol;
+			break;
+		case 4:
+			gameBoard[2][0] = symbol;
+			break;
+		case 5:
+			gameBoard[2][2] = symbol;
+			break;
+		case 6:
+			gameBoard[2][4]  = symbol;
+			break;
+		case 7:
+			gameBoard[4][0] = symbol;
+			break;
+		case 8:
+			gameBoard[4][2] = symbol;
+			break;
+		case 9:
+			gameBoard[4][4] = symbol ;
+			break;
+		default:
+			break;
+	}
+
+}
 
 
 }
